b87de0af0d7d14d24c4ae642517820bcc03df097
Pamela Torres <pamelatgtzmail.com>
2019-08-12 13:28:46 -0500

    Autenticación y logeo de cuentas

---
+++ public/js/config/ConfigFirebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyDwACSIg8suX9RAdV7h_UKETj9gA_4J9SU",
+  authDomain: "womendevs-9c071.firebaseapp.com",
+  databaseURL: "https://womendevs-9c071.firebaseio.com",
+  projectId: "womendevs-9c071",
+  storageBucket: "womendevs-9c071.appspot.com",
+  messagingSenderId: "234094838471",
+  appId: "1:234094838471:web:83fa9853d4dd595e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
